1c26ddc289bfa028fa8632a09b859dc490c7391f
nickzylstra <nick.zylstra@gmail.com>
2020-01-15 11:18:21 -0800

    Add webpack basic react build

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,20 @@
+import React, { Component } from 'react';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
+    };
+  }
+
+  render() {
+    return (
+      <>
+        hi
+      </>
+    );
+  }
+}
+
+export default App;
