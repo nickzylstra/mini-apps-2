4e2826399810e9f787783859d49594dd15e1fe85
nickzylstra <nick.zylstra@gmail.com>
2020-01-15 12:06:00 -0800

    Get events on mount

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const host = 'http://localhost:3000';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      events: [],
     };
   }
 
   async componentDidMount() {
+    this.getEvents();
+  }
 
+  async getEvents(page = 1, limit = 10) {
+    const events = await axios.get(`${host}/events`, {
+      _page: page,
+      _limit: limit,
+    });
+
+    this.setState({ events });
   }
 
   render() {
