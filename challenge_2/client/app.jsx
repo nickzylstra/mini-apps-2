46219d5ce2b49f6d6aa7b17e6221d942477d168c
nickzylstra <nick.zylstra@gmail.com>
2020-01-16 21:01:47 -0800

    Add dummy chart

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Graph from './Graph.jsx';
 
-const App = (props) => {
+const startInit = new Date('2019-12-01');
+const endInit = new Date('2019-12-31');
+
+const App = () => {
+  const [start, setStart] = useState(startInit);
+  const [end, setEnd] = useState(endInit);
 
   return (
-    <>
-      hi
-    </>
+    <div>
+      <Graph
+        start={start}
+        end={end}
+      />
+    </div>
   );
 };
 
