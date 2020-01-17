a93dbe9bc93e406d8ecbe2ec752f0dc3b68b7923
nickzylstra <nick.zylstra@gmail.com>
2020-01-17 15:09:04 -0800

    Stub out input and Scoreboard comp inputs

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Scoreboard from './components/Scoreboard.jsx';
+import RollInput from './components/RollInput.jsx';
 
 const App = () => {
+  const [scoreTotal, setScoreTotal] = useState(0);
+  const [scoreFrames, setScoreFrames] = useState(new Array(10));
+  const [frame, setFrame] = useState(1);
 
   return (
     <div>
-      hi
+      <Scoreboard
+        scoreTotal={scoreTotal}
+        scoreFrames={scoreFrames}
+      />
+      <RollInput
+        scoreTotal={scoreTotal}
+        setScoreTotal={setScoreTotal}
+        scoreFrames={scoreFrames}
+        setScoreFrames={setScoreFrames}
+        frame={frame}
+        setFrame={setFrame}
+      />
     </div>
   );
 };
