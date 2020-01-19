c971268a79006142a267c28067b351780c92abc9
nickzylstra <nick.zylstra@gmail.com>
2020-01-18 16:30:40 -0800

    Complete RollInput tests

--- challenge_3/client/components/RollInput.test.jsx
+++ challenge_3/client/components/RollInput.test.jsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import RollInput from './RollInput.jsx';
 
 describe('RollInput', () => {
-  const mockAddRoll = jest.mock();
+  const mockAddRoll = jest.fn();
   const wrapper = shallow(<RollInput
     addRoll={mockAddRoll}
   />);
@@ -21,10 +21,17 @@ describe('RollInput', () => {
     const value = 3;
     wrapper.find('input').simulate('change', { target: { value } });
     expect(wrapper.find('div.alert').getElements().length).toBe(0);
-    debugger;
     expect(wrapper.find('input').getElements()[0].props.value).toBe(value);
+
+    wrapper.find('input').simulate('change', { target: { value: '' } });
+    expect(wrapper.find('div.alert').getElements().length).toBe(0);
+    expect(wrapper.find('input').getElements()[0].props.value).toBe('');
   });
 
   test('calls addRoll on enter with valid input value', () => {
+    const value = 7;
+    wrapper.find('input').simulate('change', { target: { value } });
+    wrapper.find('input').simulate('keypress', { key: 'Enter' });
+    expect(mockAddRoll.mock.calls[0][0]).toBe(value);
   });
 });
