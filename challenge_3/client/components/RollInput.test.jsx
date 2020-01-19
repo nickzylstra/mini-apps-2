760c36489b182f03bd01728613dc0423f3f85c26
nickzylstra <nick.zylstra@gmail.com>
2020-01-18 16:19:40 -0800

    Add invalid render test

--- challenge_3/client/components/RollInput.test.jsx
+++ challenge_3/client/components/RollInput.test.jsx
@@ -13,9 +13,16 @@ describe('RollInput', () => {
   });
 
   test('renders alert for invalid input', () => {
+    wrapper.find('input').simulate('change', { target: { value: 'a' } });
+    expect(wrapper.find('div.alert').getElements().length).toBe(1);
   });
 
   test('renders valid input as input value', () => {
+    const value = 3;
+    wrapper.find('input').simulate('change', { target: { value } });
+    expect(wrapper.find('div.alert').getElements().length).toBe(0);
+    debugger;
+    expect(wrapper.find('input').getElements()[0].props.value).toBe(value);
   });
 
   test('calls addRoll on enter with valid input value', () => {
