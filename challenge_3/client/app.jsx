af58774d9228d33eb63b990593601782d86f4d25
nickzylstra <nick.zylstra@gmail.com>
2020-01-19 15:34:59 -0800

    Change state updating function to remove errors

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 import Scoreboard from './components/Scoreboard.jsx';
 import RollInput from './components/RollInput.jsx';
 
+// TODO - refactor to use Redux or alternate implementation to avoid nested state
+const scoreFramesInit = (new Array(10)).fill([null, null, null]);
 
 const App = () => {
-  const [scoreFrames, setScoreFrames] = useState((new Array(10)).fill([null, null, null]));
+  const [scoreFrames, setScoreFrames] = useState(scoreFramesInit);
   const scoreTotal = scoreFrames.reduce((total, sF) => total + sF[2], 0);
   const [frame, setFrame] = useState(1);
 
   const addRoll = (pinCount) => {
-    const nextScoreFrames = scoreFrames.slice();
+    const nextScoreFrames = [...scoreFrames].map((sF) => [...sF]);
     const scoreFramesIdx = frame - 1;
+    // debugger;
     if (!scoreFrames[scoreFramesIdx][0]) {
       nextScoreFrames[scoreFramesIdx][0] = pinCount;
       nextScoreFrames[scoreFramesIdx][2] += pinCount;
