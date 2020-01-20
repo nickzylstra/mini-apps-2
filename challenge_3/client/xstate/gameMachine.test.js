28dcf2d9f00f5941fdc9b810e65897708999f316
nickzylstra <nick.zylstra@gmail.com>
2020-01-20 15:11:16 -0800

    Config first functioning machine test

--- challenge_3/client/xstate/gameMachine.test.js
+++ challenge_3/client/xstate/gameMachine.test.js
@@ -3,11 +3,33 @@ import gameMachine from './gameMachine.js';
 
 const gameModel = createModel(gameMachine).withEvents({
   ROLL: {
-    pinCount: 3,
+    // exec: async (ctx, event) => {},
+    cases: [
+      { pinCount: 0 },
+      { pinCount: 3 },
+      { pinCount: 5 },
+      { pinCount: 7 },
+      { pinCount: 10 },
+    ],
   },
 });
 
 describe('gameMachine', () => {
+  const testPlans = gameModel.getShortestPathPlans();
+  testPlans.forEach((plan) => {
+    describe(plan.description, () => {
+      plan.paths.forEach((path) => {
+        test(path.description, async () => {
+          // TODO - add setup
+          const result = await path.test(
+            // TODO - add context
+          );
+          expect(result.state.error).toBe(null);
+        });
+      });
+    });
+  });
+
   test('has full coverage', () => {
     expect(gameModel.testCoverage()).toBe(true);
   });
