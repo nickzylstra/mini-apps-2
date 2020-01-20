c06b811a67c8515e8e1277547278b62d198f9190
nickzylstra <nick.zylstra@gmail.com>
2020-01-20 15:29:07 -0800

    Add 10frame guards

--- challenge_3/client/xstate/gameMachine/guards.js
+++ challenge_3/client/xstate/gameMachine/guards.js
@@ -4,6 +4,13 @@ const guards = {
   isStrike: (context, { pinCount }) => pinCount === 10,
   isSpare: ({ frames, currentFrame }, { pinCount }) => (
     frames[currentFrame - 1].roll1 + pinCount === 10),
+  is10Frame: ({ currentFrame }) => currentFrame === 10,
+  isStrike10Frame: ({ currentFrame }, { pinCount }) => (
+    currentFrame === 10
+      && pinCount === 10),
+  isSpare10Frame: ({ frames, currentFrame }, { pinCount }) => (
+    currentFrame === 10
+      && frames[currentFrame - 1].roll1 + pinCount === 10),
 };
 
 export default guards;
