6634f162c9b2b86997f2d6dba55b94179e5f865d
nickzylstra <nick.zylstra@gmail.com>
2020-01-31 09:48:01 -0800

    Refactor store into separate file

--- challenge_4/src/index.js
+++ challenge_4/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import  { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './redux/reducers/index';
+import store from './redux/store'
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
