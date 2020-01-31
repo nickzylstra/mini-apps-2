516cb8c912abc166269bb01c3c30b66cda77d73b
nickzylstra <nick.zylstra@gmail.com>
2020-01-31 13:56:45 -0800

    Add thunk middleware

--- challenge_4/src/redux/store/index.js
+++ challenge_4/src/redux/store/index.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleWare from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  {},
+  applyMiddleware(thunkMiddleWare),
+);
 
 export default store;
