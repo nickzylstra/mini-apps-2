83a24ac92a998c5a1a92ee5e434d9ed9ad714976
nickzylstra <nick.zylstra@gmail.com>
2020-01-31 14:53:08 -0800

    Remove init logic from grid reducer

--- challenge_4/src/redux/reducers/grid.js
+++ challenge_4/src/redux/reducers/grid.js
@@ -1,24 +1,5 @@
 
-export const rowCount = 10;
-export const colCount = 10;
-export const cellKey = 'cell';
-
-const initializeGrid = (rowCount, colCount) => {
-  const grid = [];
-  
-  for (let r = 0; r < rowCount; r += 1) {
-    grid[r] = [];
-    for (let c = 0; c < colCount; c += 1) {
-      grid[r][c] = `${cellKey}${c + r * colCount}`
-    }
-  }
-
-  return grid;
-}
-
-const initialState = initializeGrid(rowCount, colCount);
-
-const grid = (state = initialState, action) => {
+const grid = (state = [[]], action) => {
   switch (action.type) {   
     default:
       return state;
--- challenge_4/src/redux/reducers/grid.test.js
+++ challenge_4/src/redux/reducers/grid.test.js
@@ -3,8 +3,12 @@ import grid, { rowCount, colCount, cellKey } from './grid';
 describe('grid reducer', () => {
   it('should set initial state', () => {
     const initialState = grid(undefined, {});
-    expect(initialState.length).toEqual(rowCount);
-    expect(initialState[colCount - 1].length).toEqual(colCount);
-    expect(initialState[rowCount - 1][colCount - 1]).toEqual(`${cellKey}${rowCount * colCount - 1}`)
+    expect(initialState).toEqual([[]]);
   })
+  // it('should set initial state', () => {
+  //   const initialState = grid(undefined, {});
+  //   expect(initialState.length).toEqual(rowCount);
+  //   expect(initialState[colCount - 1].length).toEqual(colCount);
+  //   expect(initialState[rowCount - 1][colCount - 1]).toEqual(`${cellKey}${rowCount * colCount - 1}`)
+  // })
 })
