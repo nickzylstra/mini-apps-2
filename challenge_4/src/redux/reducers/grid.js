daaadb5c627d3d579852a599789173c4e7a42bd2
nickzylstra <nick.zylstra@gmail.com>
2020-01-31 14:27:27 -0800

    Add grid reducer with test

--- challenge_4/src/redux/reducers/grid.js
+++ challenge_4/src/redux/reducers/grid.js
@@ -1,11 +1,25 @@
-// import { CELL_UNCOVERED } from '../actions';
 
-const initialState = new Array(10).fill(new Array(10));
+export const rowCount = 10;
+export const colCount = 10;
+export const cellKey = 'cell';
+
+const initializeGrid = (rowCount, colCount) => {
+  const grid = [];
+  
+  for (let r = 0; r < rowCount; r += 1) {
+    grid[r] = [];
+    for (let c = 0; c < colCount; c += 1) {
+      grid[r][c] = `${cellKey}${c + r * colCount}`
+    }
+  }
+
+  return grid;
+}
+
+const initialState = initializeGrid(rowCount, colCount);
 
 const grid = (state = initialState, action) => {
-  switch (action.type) {
-    // case CELL_UNCOVERED:
-      
+  switch (action.type) {   
     default:
       return state;
   }
---
+++ challenge_4/src/redux/reducers/grid.test.js
@@ -0,0 +1,10 @@
+import grid, { rowCount, colCount, cellKey } from './grid';
+
+describe('grid reducer', () => {
+  it('should set initial state', () => {
+    const initialState = grid(undefined, {});
+    expect(initialState.length).toEqual(rowCount);
+    expect(initialState[colCount - 1].length).toEqual(colCount);
+    expect(initialState[rowCount - 1][colCount - 1]).toEqual(`${cellKey}${rowCount * colCount - 1}`)
+  })
+})
