9c45a554693add0fa6ca292b01227f157b946fba
nickzylstra <nick.zylstra@gmail.com>
2020-01-30 18:17:35 -0800

    Reorg dirs for redux

--- challenge_4/src/index.js
+++ challenge_4/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import  { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import rootReducer from './redux/reducers';
+import rootReducer from './redux/reducers/index';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(rootReducer);
---
+++ challenge_4/src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import {
+  // TODO
+} from '../actions/index';
+
+const rootReducer = combineReducers({
+  // TODO - add reducers
+});
+
+export default rootReducer;
