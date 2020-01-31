7e9b85871dcd01aab3a397ceac3557ab681f458c
nickzylstra <nick.zylstra@gmail.com>
2020-01-30 22:51:43 -0800

    Stub reducers notes

---
+++ challenge_4/src/redux/reducers/grid.js
@@ -0,0 +1,6 @@
+const initialState = new Array(10).fill(new Array(10));
+
+const grid = (state = initialState, action) => {
+
+  return state;
+};
--- challenge_4/src/redux/reducers/index.js
+++ challenge_4/src/redux/reducers/index.js
@@ -1,7 +1,17 @@
 import { combineReducers } from 'redux';
-import {
-  // TODO
-} from '../actions/index';
+import { types } from '../actions/index';
+
+const {
+  CLICK_CELL,
+} = types;
+
+// cellsById - { cell0: { hasMine: true, adjMineCount: 3, isCovered: true }, ... }
+// grid - array of row arrays containing cellIds [[cell0, ...], ...]
+// turns - [turn0, turn1, ...]
+// turnsById - { turn0: { row: 0, col: 3 }, ... }
+// timeElapsed - int - seconds
+// gameStatus - string - values: start, inProgress, lost, won
+// minesLeft - int
 
 const rootReducer = combineReducers({
   // TODO - add reducers
