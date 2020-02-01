fcda978329b10230bb1253824919e5f8a33362ef
nickzylstra <nick.zylstra@gmail.com>
2020-01-31 17:31:01 -0800

    Stub game and grid components

---
+++ challenge_4/src/components/Game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Grid from './Game/Grid';
+
+const Game = () => {
+
+  return (
+    <div>
+      Game
+      <Grid />
+    </div>
+  )
+};
+
+export default Game;
