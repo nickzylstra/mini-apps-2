97ebe85ec334a90bb7a4d9cc280ced7e92c42532
nickzylstra <nick.zylstra@gmail.com>
2020-01-31 17:13:14 -0800

    Add grid test

--- challenge_4/src/redux/reducers/grid.js
+++ challenge_4/src/redux/reducers/grid.js
@@ -1,6 +1,10 @@
+import * as actions from '../actions/index';
+
 
 const grid = (state = [[]], action) => {
   switch (action.type) {   
+    case actions.INITIALIZED_GAME:
+      return action.grid;
     default:
       return state;
   }
--- challenge_4/src/redux/reducers/grid.test.js
+++ challenge_4/src/redux/reducers/grid.test.js
@@ -1,8 +1,24 @@
 import grid from './grid';
+import * as actions from '../actions/index';
+
+const {
+  INITIALIZED_GAME,
+} = actions;
 
 describe('grid reducer', () => {
   it('should set initial state', () => {
     const initialState = grid(undefined, {});
     expect(initialState).toEqual([[]]);
-  })
-})
+  });
+
+  it('should set new game state', () => {
+    const state = [[]];
+    const nextGrid = [['cell1', 'cell2'], ['cell3', 'cell4']];
+    const action = {
+      type: INITIALIZED_GAME,
+      grid: nextGrid, 
+    };
+    const nextState = grid(state, action);
+    expect(nextState).toEqual(nextGrid);
+  });
+});
