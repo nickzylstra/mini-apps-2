52bbbe767b8d138e5ec22e74beb30dd81bc65a37
nickzylstra <nick.zylstra@gmail.com>
2020-02-01 17:05:59 -0800

    Add more branch coverage to clickCell

--- challenge_4/src/redux/actions/thunks/clickCell.test.js
+++ challenge_4/src/redux/actions/thunks/clickCell.test.js
@@ -70,4 +70,79 @@ describe('clickCell', () => {
     ];
     expect(actions).toEqual(expectedPayload);
   });
+
+  it('does not uncover neighbors of mine clicked cell', () => {
+    const grid = [[cellIds[0], cellIds[1]]];
+    const rowCount = 1;
+    const colCount = 2;
+    const cells = {
+      [cellIds[0]]: {
+        hasMine: false,
+        adjMineCount: 1,
+        isCovered: false,
+        isFlagged: false,
+        row: 0,
+        col: 0,
+      },
+      [cellIds[1]]: {
+        hasMine: false,
+        adjMineCount: 0,
+        isCovered: true,
+        isFlagged: false,
+        row: 0,
+        col: 1,
+      },
+    }
+    const initialState = {
+      cells,
+      grid,
+      rowCount,
+      colCount,
+    }
+    const store = mockStore(initialState);
+    store.dispatch(clickCell(cellIds[0]));
+    const actions = store.getActions();
+    const expectedPayload = [
+      { type: UNCOVERED_CELL, cellId: cellIds[0] },
+    ];
+    expect(actions).toEqual(expectedPayload);
+  });
+
+  it('uncovers neighbors of mine free clicked cell, stopping at neighbors with non-zero adjacent mine count', () => {
+    const grid = [[cellIds[0], cellIds[1]]];
+    const rowCount = 1;
+    const colCount = 2;
+    const cells = {
+      [cellIds[0]]: {
+        hasMine: false,
+        adjMineCount: 0,
+        isCovered: false,
+        isFlagged: false,
+        row: 0,
+        col: 0,
+      },
+      [cellIds[1]]: {
+        hasMine: false,
+        adjMineCount: 1,
+        isCovered: true,
+        isFlagged: false,
+        row: 0,
+        col: 1,
+      },
+    }
+    const initialState = {
+      cells,
+      grid,
+      rowCount,
+      colCount,
+    }
+    const store = mockStore(initialState);
+    store.dispatch(clickCell(cellIds[0]));
+    const actions = store.getActions();
+    const expectedPayload = [
+      { type: UNCOVERED_CELL, cellId: cellIds[0] },
+      { type: UNCOVERED_CELL, cellId: cellIds[1] },
+    ];
+    expect(actions).toEqual(expectedPayload);
+  });
 });
