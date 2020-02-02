2ab940f552ac8868de5cbd27827d4644e148efba
nickzylstra <nick.zylstra@gmail.com>
2020-02-01 21:56:38 -0800

    Add tests for uncoveredCellsRemaining reducer

---
+++ challenge_4/src/redux/reducers/uncoveredCellsRemaining.test.js
@@ -0,0 +1,38 @@
+import uncoveredCellsRemaining from './uncoveredCellsRemaining';
+import * as actions from '../actions/index';
+
+const {
+  INITIALIZED_GAME,
+  UNCOVERED_CELL,
+} = actions;
+
+describe('uncoveredCellsRemaining reducer', () => {
+  it('should set initial state', () => {
+    const initialState = uncoveredCellsRemaining(undefined, {});
+    expect(initialState).toEqual(0);
+  });
+
+  it('should set new game state', () => {
+    const state = 0;
+    const rowCount = 2;
+    const colCount = 2;
+    const mineCount = 1;
+    const action = {
+      type: INITIALIZED_GAME,
+      rowCount,
+      colCount,
+      mineCount, 
+    };
+    const nextState = uncoveredCellsRemaining(state, action);
+    expect(nextState).toEqual(3);
+  });
+
+  it('should increment count after a cell is uncovered', () => {
+    const state = 1;
+    const action = {
+      type: UNCOVERED_CELL,
+    };
+    const nextState = uncoveredCellsRemaining(state, action);
+    expect(nextState).toEqual(2);
+  });
+});
