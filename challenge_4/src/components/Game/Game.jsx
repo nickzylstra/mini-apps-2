68611bc646539850fa7ce11f77f14090db4d5545
nickzylstra <nick.zylstra@gmail.com>
2020-02-01 23:32:41 -0800

    Update UI to reflect game status

--- challenge_4/src/components/Game/Game.jsx
+++ challenge_4/src/components/Game/Game.jsx
@@ -15,22 +15,18 @@ const Game = () => {
   const rowCount = useSelector(({ rowCount }) => rowCount);
   const colCount = useSelector(({ colCount }) => colCount);
   const gameStatus = useSelector(({ gameStatus }) => gameStatus);
-  const openCellsLeft = useSelector(({ uncoveredCellsRemaining }) => uncoveredCellsRemaining);
   const style = {
     width: `${colCount * 18 + 20}px`,
-    height: `${rowCount * 18 + 50}px`,
+    height: `${rowCount * 18 + 32}px`,
   };
 
   return (
     <div className="game" style={style}>
-      Minesweeper
-      <div>
-        status:
-        {gameStatus}
-      </div>
-      <div>
-        openCellsLeft:
-        {openCellsLeft}
+      <div
+        onClick={() => dispatch(initializeGame())}
+        className="smiley"
+        >
+        {`Minesweeper  ${gameStatus === 'LOST' ? ':(' : ':)'}`}
       </div>
       <Grid
         rowCount={rowCount}
--- challenge_4/src/components/Game/Grid/Cell.jsx
+++ challenge_4/src/components/Game/Grid/Cell.jsx
@@ -7,13 +7,16 @@ import './Cell.css'
 const Cell = ({ cellId }) => {
   const dispatch = useDispatch();
   const cell = useSelector(({ cells }) => cells[cellId]);
+  const gameStatus = useSelector(({ gameStatus }) => gameStatus);
   const { adjMineCount, isCovered, hasMine, isFlagged } = cell;
   return (
     <div
       id={cellId}
       onClick={(e) => { dispatch(clickCell(e.target.id)); }}
-      className={`cell ${isCovered ? 'blank' : 'exposed'}
-        ${hasMine ? 'mine' : ''} ${isFlagged ? 'flagged' : ''}`}
+      className={`cell
+        ${isCovered ? 'blank' : 'exposed'}
+        ${hasMine && (gameStatus === 'WON' || gameStatus === 'LOST') ? 'mine' : ''}
+        ${isFlagged ? 'flagged' : ''}`}
     >
       {isCovered ? '.' : adjMineCount || '.'}
     </div>
