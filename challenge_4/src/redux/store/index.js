157cde6f34e6e5a7598a0e0ad676759c5b7e0e67
nickzylstra <nick.zylstra@gmail.com>
2020-02-01 22:14:50 -0800

    Add devtools

--- challenge_4/src/redux/store/index.js
+++ challenge_4/src/redux/store/index.js
@@ -1,11 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleWare from 'redux-thunk';
 import rootReducer from '../reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
   {},
-  applyMiddleware(thunkMiddleWare),
+  composeEnhancers(applyMiddleware(thunkMiddleWare)),
 );
 
 export default store;
