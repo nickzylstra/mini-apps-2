1665f3d104f048411a46518f6dfde4688aacd2a2
nickzylstra <nick.zylstra@gmail.com>
2020-02-01 23:19:19 -0800

    Add clickCell test coverage for win and loss

--- challenge_4/src/redux/actions/thunks/clickCell.test.js
+++ challenge_4/src/redux/actions/thunks/clickCell.test.js
@@ -1,6 +1,8 @@
 import mockStore from '../../../../testUtils/mockStore';
 import { clickCell } from './clickCell';
 import { UNCOVERED_CELL } from '../index';
+import { WON_GAME } from '../winGame';
+import { LOST_GAME } from '../loseGame';
 
 
 describe('clickCell', () => {
@@ -153,4 +155,67 @@ describe('clickCell', () => {
     ];
     expect(actions).toEqual(expectedPayload);
   });
+
+  it('dispatches win after all open cells uncovered', () => {
+    const grid = [[cellIds[0]]];
+    const rowCount = 1;
+    const colCount = 1;
+    const cells = {
+      [cellIds[0]]: {
+        hasMine: false,
+        adjMineCount: 0,
+        isCovered: true,
+        isFlagged: false,
+        row: 0,
+        col: 0,
+      },
+    }
+    const uncoveredCellsRemaining = 1;
+    const initialState = {
+      cells,
+      grid,
+      rowCount,
+      colCount,
+      uncoveredCellsRemaining,
+    }
+    const store = mockStore(initialState);
+    store.dispatch(clickCell(cellIds[0]));
+    const actions = store.getActions();
+    const expectedPayload = [
+      { type: UNCOVERED_CELL, cellId: cellIds[0] },
+      { type: WON_GAME },
+    ];
+    expect(actions).toEqual(expectedPayload);
+  });
+
+  it('dispatches loss when clicking a mine', () => {
+    const grid = [[cellIds[0]]];
+    const rowCount = 1;
+    const colCount = 1;
+    const cells = {
+      [cellIds[0]]: {
+        hasMine: true,
+        adjMineCount: 0,
+        isCovered: true,
+        isFlagged: false,
+        row: 0,
+        col: 0,
+      },
+    }
+    const uncoveredCellsRemaining = 1;
+    const initialState = {
+      cells,
+      grid,
+      rowCount,
+      colCount,
+      uncoveredCellsRemaining,
+    }
+    const store = mockStore(initialState);
+    store.dispatch(clickCell(cellIds[0]));
+    const actions = store.getActions();
+    const expectedPayload = [
+      { type: LOST_GAME },
+    ];
+    expect(actions).toEqual(expectedPayload);
+  });  
 });
