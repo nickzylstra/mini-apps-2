07c41136fc0f093d26ee6da0ce86261867548fdc
nickzylstra <nick.zylstra@gmail.com>
2020-02-03 17:47:00 -0800

    Position Smiley

--- challenge_4/src/components/Game/Header/Smiley.jsx
+++ challenge_4/src/components/Game/Header/Smiley.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { initializeGame, statuses } from '../../../redux/actions';
+import { initializeGame } from '../../../redux/actions';
 import './Smiley.css'
 
 
@@ -9,10 +9,13 @@ const Smiley = () => {
   const gameStatus = useSelector(({ gameStatus }) => gameStatus);
 
   return (
-    <div
-      onClick={() => dispatch(initializeGame())}
-      className={`smiley ${gameStatus}`}
-    >
+    <div className="smileyContainer">
+      <div>
+        <div
+          onClick={() => dispatch(initializeGame())}
+          className={`smiley ${gameStatus}`}
+        />
+      </div>
     </div>
   )
 };
