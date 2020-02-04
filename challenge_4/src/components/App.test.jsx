3d7a667c57a8fb813c1cf17348aafa822b56ea87
nickzylstra <nick.zylstra@gmail.com>
2020-02-03 22:36:44 -0800

    Add app tests back

--- challenge_4/src/components/App.test.jsx
+++ challenge_4/src/components/App.test.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import renderWithRedux from '../../testUtils/renderWithRedux';
+import renderWithRedux from '../../testUtils/renderWithRedux';
 import App from './App.jsx';
 
 
 describe('App', () => {
   test('renders game title', () => {
-    // const { getByTitle } = renderWithRedux(<App />);
-    // const title = getByTitle(/game/);
-    // expect(title).toBeInTheDocument();
+    const { getByTitle } = renderWithRedux(<App />);
+    const title = getByTitle(/game/);
+    expect(title).toBeInTheDocument();
   });
 })
